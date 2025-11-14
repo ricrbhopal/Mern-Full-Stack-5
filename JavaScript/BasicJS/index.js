00ae82345267713a3f56ea4c7ac11868eb6a39ea
Raj Vardhan <errvmaurya563@gmail.com>
2025-11-14 20:28:12 +0530

    Add initial HTML structure, CSS styles, and JavaScript functionality for various components

---
+++ JavaScript/BasicJS/index.js
@@ -0,0 +1,21 @@
+var a = 10;
+
+var a = 20;
+
+let b = 10;
+
+console.log(a, b);
+
+let ar = ["Abc", 1235, 45.67, "a", true];
+console.log(ar);
+
+alert("this is alert");
+
+console.warn("this is warning");
+console.error("ye error hai");
+
+if (confirm("Are you Sure")) {
+  console.log("thanks");
+} else {
+  console.error("Its ok");
+}
