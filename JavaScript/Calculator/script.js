c94fbf13c097747308b56f80ea7282c8b44b3771
Raj Vardhan <errvmaurya563@gmail.com>
2025-11-23 15:54:54 +0530

    Add calculator HTML structure, CSS styles, and JavaScript functionality

---
+++ JavaScript/Calculator/script.js
@@ -0,0 +1,9 @@
+function Input(char) {
+  if (char === "=") {
+    document.getElementById("display").value = "result";
+  } else if (char === "C") {
+    document.getElementById("display").value = "";
+  } else {
+    document.getElementById("display").value = char;
+  }
+}
