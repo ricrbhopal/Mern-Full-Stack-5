dcddd278a18c5ddd53c62e7cddf5e4fc5a664c83
Raj Vardhan <errvmaurya563@gmail.com>
2025-11-30 18:19:30 +0530

    Add image editing features: implement grayscale, sepia, and invert filters with corresponding controls

--- JavaScript/ImageEditor/script.js
+++ JavaScript/ImageEditor/script.js
@@ -1,3 +1,9 @@
+let b = 1,
+  c = 1,
+  g = 0,
+  s = 0,
+  i = 0;
+
 const img = document.getElementById("image").src;
 
 console.log(img);
@@ -6,7 +12,6 @@ if (img === "http://127.0.0.1:5500/JavaScript/ImageEditor/index.html") {
   document.getElementById("image").style.display = "none";
 }
 
-
 function uploadImage() {
   const file = document.getElementById("Upload").files[0];
   const fileURL = URL.createObjectURL(file);
@@ -14,16 +19,58 @@ function uploadImage() {
   document.getElementById("image").src = fileURL;
   document.getElementById("image").style.display = "block";
   document.getElementById("uploadLabel").style.display = "none";
+  applyFilter();
 }
 
-
-function changeBrightness(){
-    const value =  document.getElementById("Brightness").value;
-    document.getElementById("image").style.filter=`brightness(${value*2/100})`
+function applyFilter() {
+  document.getElementById("image").style.filter = ` brightness(${b}) 
+                                                    contrast(${c}) 
+                                                    grayscale(${g}%) 
+                                                    sepia(${s}%)
+                                                    invert(${i}%)`;
 }
 
+function changeBrightness() {
+  const value = document.getElementById("Brightness").value;
+  b = (value * 2) / 100;
+  applyFilter();
+}
 
-function changeContrast(){
-    const value =  document.getElementById("Contrast").value;
-    document.getElementById("image").style.filter=`contrast(${value*2/100})`
+function changeContrast() {
+  const value = document.getElementById("Contrast").value;
+  c = (value * 2) / 100;
+  applyFilter();
+}
+
+function changeGrayscale() {
+  const value = document.getElementById("Grayscale").value;
+  g = value;
+  applyFilter();
+}
+
+function changeSepia() {
+  const value = document.getElementById("Sepia").value;
+  s = value;
+  applyFilter();
+}
+
+function changeInvert() {
+  const value = document.getElementById("Invert").value;
+  i = value;
+  applyFilter();
+}
+
+function reset() {
+  b = 1;
+  c = 1;
+  g = 0;
+  s = 0;
+  i = 0;
+
+  applyFilter();
+  document.getElementById("Brightness").value = "50";
+  document.getElementById("Contrast").value = "50";
+  document.getElementById("Sepia").value = "0";
+  document.getElementById("Invert").value = "0";
+  document.getElementById("Grayscale").value = "0";
 }
