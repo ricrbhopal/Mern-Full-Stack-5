78425666b97b7d80e0bac3d740f1109c1fb2b16f
Raj Vardhan <errvmaurya563@gmail.com>
2025-12-23 00:15:57 +0530

    feat: enhance weather display with icons and improve error handling in script

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -1,15 +1,15 @@
 async function getWeather() {
-  const city = document.getElementById("city").value.trim();
-  const { lat, lon } = await getGeoLoc(city);
-  console.log(lat, lon);
+  try {
+    const city = document.getElementById("city").value.trim();
+    const { lat, lon } = await getGeoLoc(city);
+    console.log(lat, lon);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=1cefe43006e662107c8d0454bde6852c`
+    );
 
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=1cefe43006e662107c8d0454bde6852c`
-  );
+    const data = await response.json();
 
-  const data = await response.json();
-
-  document.getElementById("WeatherData").innerHTML = `
+    document.getElementById("WeatherData").innerHTML = `
            <div>
             <p>Temperature : ${(data.main.temp - 273.14).toFixed(2)}ºC</p>
             <p>Humidity : ${data.main.humidity}%</p>
@@ -19,18 +19,25 @@ async function getWeather() {
             data.weather[0].icon
           }@4x.png" alt="WeatherIcon" />
   `;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function getGeoLoc(City) {
-  console.log(City);
-  const response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${City}&limit=1&appid=1cefe43006e662107c8d0454bde6852c`
-  );
+  try {
+    console.log(City);
+    const response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${City}&limit=1&appid=1cefe43006e662107c8d0454bde6852c`
+    );
 
-  const data = await response.json();
+    const data = await response.json();
 
-  const lat = data[0].lat;
-  const lon = data[0].lon;
+    const lat = data[0].lat;
+    const lon = data[0].lon;
 
-  return { lat, lon };
+    return { lat, lon };
+  } catch (error) {
+    throw error;
+  }
 }
