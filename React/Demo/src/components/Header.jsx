a7acc05bee00723cbe69fe2cd6f5a2ef9793ee1f
Raj Vardhan <errvmaurya563@gmail.com>
2025-12-28 17:57:50 +0530

    feat: enhance Contact page with form handling and validation

--- React/Demo/src/components/Header.jsx
+++ React/Demo/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 const Header = () => {
+
   return (
     <>
       <div className="d-flex justify-content-between align-items-center p-2 bg-primary text-light">
