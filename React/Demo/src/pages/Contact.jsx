98f562e3aa08704cc9cfc19fa4bd8152cd10bd1b
Raj Vardhan <errvmaurya563@gmail.com>
2025-12-29 19:22:47 +0530

    feat: refactor Contact component to use a single state object for form data management

--- React/Demo/src/pages/Contact.jsx
+++ React/Demo/src/pages/Contact.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 
 const Contact = () => {
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [contactData, setContactData] = useState({
+    fullName: "",
+    email: "",
+    phone: "",
+    city: "",
+    subject: "",
+    message: "",
+  });
+
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleClearForm = () => {
-    setFullName("");
-    setEmail("");
-    setMessage("");
+  const handleChange = (e) => {
+    const { name, value } = e.traget;
+    setContactData((previousData) => ({ ...previousData, [name]: value }));
   };
+
+  const handleClearForm = () => {};
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,12 +26,6 @@ const Contact = () => {
       const response = await fetch(
         "https://official-joke-api.appspot.com/jokes/jhbaskdjbf"
       );
-      const data = {
-        fullName,
-        email,
-        message,
-      };
-      console.log(data);
     } catch (error) {
       console.log(error.message);
     } finally {
@@ -45,31 +46,72 @@ const Contact = () => {
                 type="text"
                 name="fullName"
                 id="fullName"
-                value={fullName}
-                onChange={(event) => setFullName(event.target.value)}
+                value={contactData.fullName}
+                onChange={handleChange}
                 placeholder="Enter your Name"
                 className="text-primary"
               />
             </div>
+
             <div>
               <label htmlFor="email">Email</label>
               <input
                 type="email"
                 name="email"
                 id="email"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
+                value={contactData.email}
+                onChange={handleChange}
                 placeholder="Enter your Email"
                 className="text-primary"
               />
             </div>
+
+            <div>
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="number"
+                name="phone"
+                id="phone"
+                value={contactData.phone}
+                onChange={handleChange}
+                placeholder="Enter your phone"
+                className="text-primary"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="city">City</label>
+              <input
+                type="text"
+                name="city"
+                id="city"
+                value={contactData.city}
+                onChange={handleChange}
+                placeholder="Enter your city"
+                className="text-primary"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="subject">Subject</label>
+              <input
+                type="text"
+                name="subject"
+                id="subject"
+                value={contactData.subject}
+                onChange={handleChange}
+                placeholder="Enter your subject"
+                className="text-primary"
+              />
+            </div>
+
             <div>
               <label htmlFor="message">Message</label>
               <textarea
                 name="message"
                 id="message"
-                value={message}
-                onChange={(event) => setMessage(event.target.value)}
+                value={contactData.message}
+                onChange={handleChange}
                 placeholder="Enter your Message"
                 className="text-primary"
               ></textarea>
