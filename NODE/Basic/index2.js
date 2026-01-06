22d6938d8afddc9776f808142afef455e83346c7
Raj Vardhan <errvmaurya563@gmail.com>
2026-01-06 20:50:49 +0530

    chore: initialize package.json for firstBackend with basic configuration and dependencies

---
+++ NODE/Basic/index2.js
@@ -0,0 +1,10 @@
+// import { add, sub } from "./index.js";
+// import kuchBhi from "./index.js";
+import div from "./index.js";
+
+let x = 10;
+let y = 20;
+
+div(x, y);
+// sub(y, x);
+// kuchBhi(x, y);
