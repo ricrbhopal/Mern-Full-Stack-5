e5a1dc9f6c5c8b90a87dc89d06d031dae211a21a
Raj Vardhan <errvmaurya563@gmail.com>
2026-01-11 19:14:50 +0530

    feat: implement registration form with validation and API integration

--- Cravings/server/index.js
+++ Cravings/server/index.js
@@ -1,11 +1,13 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import connectDB from './src/config/db.js';
+import cors from "cors";
+import connectDB from "./src/config/db.js";
 
 const app = express();
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
 // app.use("/auth", AuthRouter);
@@ -17,5 +19,5 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server Started at Port: ", port);
-  connectDB()
+  connectDB();
 });
