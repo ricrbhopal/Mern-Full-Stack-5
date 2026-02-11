a5ac034d8cbc631e26ef4d1c04e8317cb06f6b8b
Raj Vardhan <errvmaurya563@gmail.com>
2026-02-11 21:00:59 +0530

    feat: initialize React client with Vite setup
    - Added package.json with dependencies for React, Vite, and ESLint.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the application.
    - Implemented App.jsx as the main component with state management.
    - Added React logo SVG in assets directory.
    - Created index.css for global styles.
    - Set up main.jsx as the entry point for rendering the application.
    - Configured Vite with a basic vite.config.js file.

--- AdvanceJavaScript/callbacks/script.js
+++ AdvanceJavaScript/callbacks/script.js
@@ -10,23 +10,23 @@
 // console.log(9);
 // console.log(10);
 
-// function abc() {
-//   setTimeout(() => {
-//     console.log("Hello");
-//   }, 5000);
-// }
+function abc() {
+  setTimeout(() => {
+    console.log("Hello");
+  }, 5000);
+}
 
-// function sum(a, b) {
-//   x = a + 1;
-//   y = b + 1;
-//   return x + y;
-// }
+function sum(a, b) {
+  x = a + 1;
+  y = b + 1;
+  return x + y;
+}
 
-// function Addition(p, q, JOR) {
-//   m = p + 1;
-//   n = q - 1;
-//   let result = JOR(m, n);
-//   console.log(result);
-// }
+function Addition(p, q, JOR) {
+  m = p + 1;
+  n = q - 1;
+  let result = JOR(m, n);
+  console.log(result);
+}
 
-// Addition(2, 3, sum);
+Addition(2, 3, sum);
